281d0263ea31d4837ccc6c56c66972bd53742c25
Witherman3 <132720887+Witherman3@users.noreply.github.com>
2023-05-05 20:32:42 -0400

    Update config.js

--- config.js
+++ config.js
@@ -54,12 +54,12 @@ const config = {
   ),
   //----------------------------------------------------------------
   // Your social handle links
-  githubLink: "https://github.com/mohitmimani",
-  twitterLink: "https://twitter.com",
-  mediumLink: "https://medium.com/",
-  facebookLink: "https://facebook.com",
-  discordLink: "https://discord.com",
-  youtubeLink: "https://www.youtube.com/channel/UCfhRHeex9lU0ZAVJot6Z2ZQ",
+
+
+
+
+  discordLink: "https://discord.gg/S9MNuvqtxQ",
+  youtubeLink: "https://youtube.com/@greatlake_editz",
 };
 
 export default config;
